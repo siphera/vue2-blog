429e386ad453e1f16a826082092d385f1c9753b6
siphera <salmansiphenkosi@gmail.com>
2021-05-27 15:25:48 +0200

    footer done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, Javascript and more. Register today!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPosts: [
+        {
+          title: "This is a filler title",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
