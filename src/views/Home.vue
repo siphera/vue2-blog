45b6aeaa8bedcc9b4f449a0dd7a0ef0c195651fa
siphera <salmansiphenkosi@gmail.com>
2021-05-27 16:25:14 +0200

    blog post component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+  </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost'
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
   data() {
     return {
       welcomeScreen: {
@@ -15,9 +19,14 @@ export default {
         welcomeScreen: true,
         photo: "coding",
       },
-      sampleBlogPosts: [
+      sampleBlogPost: [
         {
           title: "This is a filler title",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+        {
+          title: "This is a filler title2",
           blogHTML: "This is a filler blog post title!",
           blogCoverPhoto: "designed-for-everyone",
         },
